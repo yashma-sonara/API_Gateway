bb530c214e1561bdacf19c722b08e61762e5d7f4
JiaXinEu <108446221+JiaXinEu@users.noreply.github.com>
2023-06-02 01:02:10 +0800

    Create main.go in HertzDemo

---
+++ Hertz_Demo/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+)
+
+func decode(c context.Context, ctx *app.RequestContext) {
+	path := ctx.Request.Path()
+	pathStr := string(path)
+	splitArr := strings.Split(pathStr, "/")
+	if len(splitArr) < 3 {
+		log.Println("Invalid URL path:", path)
+		ctx.SetStatusCode(http.StatusBadRequest)
+		ctx.WriteString("Invalid URL path")
+		return
+	}
+
+	serviceName := splitArr[1]
+	method := splitArr[2]
+	directMethod := ctx.Request.Method()
+	fmt.Println("Service name: ", serviceName, "Method", method, "/", directMethod)
+}
+
+func main() {
+	hz := server.Default(
+		server.WithHostPorts("127.0.0.1:8080"),
+	)
+
+	hz.GET("/", decode)
+
+	hz.Spin()
+}
