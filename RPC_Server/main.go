7c85d86cecdf1b3721f41dd252c77bede37bd1a1
tis8cjiaxineu@gmail.com <tis8cjiaxineu@gmail.com>
2023-06-14 23:56:55 +0800

    Update main.go and nacos.go, remove redundant code

--- RPC_Server/main.go
+++ RPC_Server/main.go
@@ -21,29 +21,19 @@ func main() {
 	numInstances := 3
 	count := 0
 
-	// Create a wait group to wait for all instances to start
 	var group sync.WaitGroup
 	group.Add(numInstances)
 
-	// Start multiple instances in Goroutines
 	for i := 0; i < numInstances; i++ {
 		go func(instanceID int) {
 			defer group.Done()
 
-			fmt.Println(count)
 			addr, _ := net.ResolveTCPAddr("tcp", fmt.Sprintf("127.0.0.1:808%d", count))
 			count++
-
-			// r, err := registry.NewDefaultNacosRegistry()
-			// if err != nil {
-			// 	panic(err)
-			// }
 
 			svr := api.NewServer(
 				new(ServiceAImpl),
 				server.WithServiceAddr(addr),
-				// server.WithRegistry(r),
-				// server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "ServiceA"}),
 			)
 
 			err1 := svr.Run()
@@ -53,11 +43,6 @@ func main() {
 			}
 		}(i)
 	}
-
-	// svr := api.NewServer(
-	// 	new(ServiceAImpl))
-
-	// err := svr.Run()
 	group.Wait()
 
 }
