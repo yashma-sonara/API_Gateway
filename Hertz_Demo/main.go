f7ee613c6d6c7643487c95eb51e1fb756a94d939
tis8cjiaxineu@gmail.com <tis8cjiaxineu@gmail.com>
2023-06-25 21:17:41 +0800

    Update main.go with go doc comments

--- Hertz_Demo/main.go
+++ Hertz_Demo/main.go
@@ -1,3 +1,11 @@
+// Package main provides an API Gateway server that handles
+// incoming requests and performs operations such as request validation,
+// protocol translation, service discovery, load balancing and request forwarding.
+//
+// The main package sets up a Hertz server and defines
+// a handler function, `decode`, to handle incoming requests. The `decode`
+// function performs the necessary operations to handle and forward the request and
+// returns the response or an error if any operation fails.
 package main
 
 import (
@@ -13,43 +21,123 @@ import (
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/cloudwego/kitex/client"
 	"github.com/cloudwego/kitex/client/genericclient"
+	"github.com/cloudwego/kitex/pkg/discovery"
 	"github.com/cloudwego/kitex/pkg/generic"
 	"github.com/cloudwego/kitex/pkg/loadbalance"
 	"github.com/kitex-contrib/registry-nacos/resolver"
 )
 
-func translate(ctx *app.RequestContext) generic.Generic {
-	p, err5 := generic.NewThriftFileProvider("../RPC_Server/serviceA.thrift")
-	if err5 != nil {
-		log.Println("Error", err5.Error())
-		ctx.SetStatusCode(http.StatusBadGateway)
-		ctx.AbortWithStatus(502)
-	}
+// type GenericServiceImpl struct {
+// }
 
-	g, err6 := generic.JSONThriftGeneric(p)
-	if err6 != nil {
-		panic(err6)
-	}
-	return g
+//	func (g *GenericServiceImpl) GenericCall(ctx context.Context, method string, request interface{}) (response interface{}, err error) {
+//		// use jsoniter or other JSON parsing library to assert request
+//		m := request.(string)
+//		fmt.Printf("Recv: %v\n", m)
+//		return "{\"Msg\": \"world\"}", nil
+//	}
+
+// validateContentType checks if the content type of ctx is valid.
+// It returns true if the content type is invalid, otherwise false.
+func validateContentType(ctx *app.RequestContext) bool {
+	return string(ctx.ContentType()) != "application/json"
 }
 
+// readPath reads the ctx request path and returns an array
+// of the path splitted into segments by "/"
+func readPath(ctx *app.RequestContext) []string {
+	path := ctx.Request.Path()
+	pathStr := string(path)
+	splitArr := strings.Split(pathStr, "/")
+	return splitArr
+}
+
+// parseRequestBody parses the request body into a JSON map using Unmarshal.
+// It returns the parsed JSON map and an error if parsing fails.
+func parseRequestBody(body []byte) (map[string]interface{}, error) {
+	var jsonMap map[string]interface{}
+	err := json.Unmarshal(body, &jsonMap)
+	return jsonMap, err
+}
+
+// getServiceName returns the service name requested from arr.
+//
+// Index out of range error if length of arr is lesser than 2.
+func getServiceName(arr []string) string {
+	return arr[1]
+}
+
+// getMethod returns the method name requested from  arr.
+//
+// Index out of range error if length of arr is lesser than 3.
+func getMethod(arr []string) string {
+	return arr[2]
+}
+
+// createNacosRegistry creates a new default Nacos service resolver.
+// It returns the Nacos resolver and an error if creation fails.
+func createNacosRegistry() (discovery.Resolver, error) {
+	return resolver.NewDefaultNacosResolver()
+}
+
+// resolveService resolves serviceName and c using the Nacos resolver re.
+// It returns the result of the service discovery process and an error if resolution fails.
+func resolveService(c context.Context, re discovery.Resolver, serviceName string) (discovery.Result, error) {
+	return re.Resolve(c, serviceName)
+}
+
+// checkInstances prints the number of instances found in result to log.
+func checkInstances(result discovery.Result) {
+	instances := result.Instances
+	log.Println("Number of instances found:", len(instances))
+	for index, instance := range instances {
+		log.Println("Instance", index, "at address", instance.Address())
+	}
+}
+
+// translateThrift translates the JSON body in ctx to Thrift.
+// The provided IDL file is at path ../RPC_Server/serviceA.thrift
+// It returns the translated Generic object and an error if file parse or translation fails.
+func translateThrift(ctx *app.RequestContext) (generic.Generic, error) {
+	p, err := generic.NewThriftFileProvider("../RPC_Server/serviceA.thrift")
+	if err != nil {
+		return nil, err
+	}
+	g, err := generic.JSONThriftGeneric(p)
+	if err != nil {
+		return nil, err
+	}
+	return g, nil
+}
+
+// makeGenericCall performs generic call on c and body,
+// using the generic client cli to the specified method.
+// It returns the response from the call and an error if the call fails.
+func makeGenericCall(c context.Context, cli genericclient.Client, method string, body string) (interface{}, error) {
+	return cli.GenericCall(c, method, body)
+}
+
+// decode handles the incoming request and performs the necessary operations.
+// It validates the context ctx, parses the request body, discover the service,
+// and makes a generic call with load balancer. Finally, it returns the response in JSON, or an error if any operation fails.
 func decode(c context.Context, ctx *app.RequestContext) {
-	if string(ctx.ContentType()) != "application/json" {
-		log.Println("Invalid Content-Type:", ctx.ContentType())
+	if validateContentType(ctx) {
+		log.Println("Invalid Content-Type:", string(ctx.ContentType()))
 		ctx.SetStatusCode(http.StatusBadRequest)
 		ctx.String(consts.StatusBadRequest, "Invalid Content-Type, expected application/json")
 		return
 	}
 
-	path := ctx.Request.Path()
-	pathStr := string(path)
-	splitArr := strings.Split(pathStr, "/")
+	splitArr := readPath(ctx)
 	if len(splitArr) < 3 {
-		log.Println("Invalid URL path:", path)
+		log.Println("Invalid URL path:", ctx.Request.Path())
 		ctx.SetStatusCode(http.StatusBadRequest)
-		ctx.String(consts.StatusBadGateway, "Invalid URL path")
+		ctx.String(consts.StatusBadRequest, "Invalid URL path")
 		return
 	}
+
+	serviceName := getServiceName(splitArr)
+	method := getMethod(splitArr)
 
 	body, err := ctx.Body()
 	if err != nil {
@@ -59,51 +147,88 @@ func decode(c context.Context, ctx *app.RequestContext) {
 		return
 	}
 
-	var jsonMap map[string]interface{}
-	err2 := json.Unmarshal(body, &jsonMap)
-	if err2 != nil {
-		log.Println("Error parsing JSON:", err2)
+	// if len(body) == 0 {
+	// 	log.Println("Empty request body")
+	// 	ctx.SetStatusCode(http.StatusBadRequest)
+	// 	ctx.WriteString("Empty request body")
+	// 	return
+	// }
+
+	// fmt.Println(string(body))
+
+	_, err = parseRequestBody(body)
+	if err != nil {
+		log.Println("Error parsing JSON:", err)
 		ctx.SetStatusCode(http.StatusBadRequest)
 		ctx.String(consts.StatusBadRequest, "Invalid JSON data")
 		return
 	}
 
-	serviceName := splitArr[1]
-	method := splitArr[2]
+	// httpMethod := ctx.Request.Method()
+	// fmt.Println("Service name:", serviceName, ", Method:", method, "/", string(httpMethod))
+	// fmt.Println(jsonMap)
 
-	re, err3 := resolver.NewDefaultNacosResolver()
-	if err3 != nil {
-		log.Println("Error creating new Nacos Resolver:", err3.Error())
+	re, err := createNacosRegistry()
+	if err != nil {
+		log.Println("Error creating new Nacos Resolver:", err)
 		ctx.SetStatusCode(http.StatusInternalServerError)
 		ctx.String(consts.StatusInternalServerError, "Error creating new Nacos Resolver")
+		return
 	}
 
-	// Discover number of instances registered
-	// result, err4 := re.Resolve(c, serviceName)
-	// if err4 != nil {
-	// 	log.Println("Error", err4.Error())
-	// }
+	// endPointInfo := rpcinfo.NewEndpointInfo(serviceName, method, nil, nil)
+	// t := re.Target(c, endPointInfo)
 
-	// instances := result.Instances
-	// fmt.Println("Number of instances found: ", len(instances))
-	// for index, instance := range instances {
-	// 	fmt.Println("Instance", index, "at address", instance.Address())
-	// }
-
-	// Create the generic client
-	g := translate(ctx)
-
-	cli, err7 := genericclient.NewClient(serviceName, g, client.WithResolver(re), client.WithLoadBalancer(loadbalance.NewWeightedRandomBalancer()))
-	if err7 != nil {
-		panic(err7)
+	result, err := resolveService(c, re, serviceName)
+	if err != nil {
+		log.Println("Error resolving service:", err)
+		ctx.SetStatusCode(http.StatusInternalServerError)
+		ctx.String(consts.StatusInternalServerError, "Error resolving service")
+		return
 	}
 
-	fmt.Println(string(body))
+	checkInstances(result)
 
-	// Make a request to the server
-	resp, err8 := cli.GenericCall(c, method, string(body))
-	if err8 != nil {
-		ctx.JSON(consts.StatusBadRequest, err8.Error())
+	// re, err5 := resolver.NewDefaultNacosResolver()
+	// if err5 != nil {
+	// 	log.Println("Error creating new Nacos Resolver:", err5.Error())
+	// }
+
+	g, err := translateThrift(ctx)
+	if err != nil {
+		log.Println("Error translating Thrift:", err)
+		ctx.SetStatusCode(http.StatusInternalServerError)
+		ctx.String(consts.StatusInternalServerError, "Error translating Thrift")
+		return
+	}
+
+	cli, err := genericclient.NewClient(serviceName, g, client.WithResolver(re), client.WithLoadBalancer(loadbalance.NewWeightedRandomBalancer()))
+	if err != nil {
+		log.Println("Error creating generic client:", err)
+		ctx.SetStatusCode(http.StatusInternalServerError)
+		ctx.String(consts.StatusInternalServerError, "Error creating generic client")
+		return
+	}
+
+	// Create the server
+	// svr := genericserver.NewServer(new(GenericServiceImpl), g)
+	// // Start the server
+	// go func() {
+	// 	err := svr.Run()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+
+	// Wait for the server to start
+	// time.Sleep(time.Second)
+	// fmt.Println(string(body))
+
+	resp, err := makeGenericCall(c, cli, method, string(body))
+	if err != nil {
+		log.Println("Error making generic call:", err)
+		ctx.SetStatusCode(http.StatusInternalServerError)
+		ctx.String(consts.StatusInternalServerError, "Error making generic call")
 		return
 	}
 
@@ -111,6 +236,10 @@ func decode(c context.Context, ctx *app.RequestContext) {
 	ctx.JSON(consts.StatusOK, resp)
 }
 
+// main acts as the entry point of the server application. It sets up a server
+// using the Hertz framework and registers the `decode` function as the
+// handler for incoming requests. The server listens on 127.0.0.1:8888
+// and handles requests for any registered routes.
 func main() {
 	hz := server.Default(
 		server.WithHostPorts("127.0.0.1:8888"),
