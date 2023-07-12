a9e561f32eaf370d38ec281d167a3e6810d18077
Yashma Sonara <44407952+yashma-sonara@users.noreply.github.com>
2023-07-13 02:04:48 +0800

    Update tests

--- API_Gateway_Server/main_test.go
+++ API_Gateway_Server/main_test.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol"
 	"github.com/stretchr/testify/assert"
 )
-
 
 func TestDecode_ValidRequest(t *testing.T) {
 	body := `{"userId": "test id", "message": "test"}`
@@ -22,6 +22,7 @@ func TestDecode_ValidRequest(t *testing.T) {
 
 	testC := context.Background()
 
+	initialise()
 	decode(testC, ctx)
 
 	assert.Equal(t, http.StatusOK, ctx.Response.StatusCode())
@@ -107,7 +108,6 @@ func TestIntegration1_UpdateCorrectIDL(t *testing.T) {
 	assert.Equal(t, expected, string(ctx.Response.Body()))
 }
 
-
 func TestIntegration2_UpdateIncorrectIDL(t *testing.T) {
 	body := `{"userId": "test id", "file": "../RPC_Server/serviceA3.thrift"}`
 	ctx := &app.RequestContext{
@@ -125,7 +125,6 @@ func TestIntegration2_UpdateIncorrectIDL(t *testing.T) {
 	expected := "Internal server error, fail to update IDL"
 	assert.Equal(t, expected, string(ctx.Response.Body()))
 }
-
 
 func TestIntegration3_IncorrectService(t *testing.T) {
 	body := `{"userId": "test id", "message": "test"}`
@@ -147,6 +146,34 @@ func TestIntegration3_IncorrectService(t *testing.T) {
 }
 
 func TestIntegration4_ValidRequestWithUpdatedIDL(t *testing.T) {
+	initialise()
+	// Read the content of the new thrift file
+	newThriftFile := "../RPC_Server/serviceA2.thrift"
+	newContent, err := os.ReadFile(newThriftFile)
+	assert.NoError(t, err)
+
+	// Write the new content to the existing thrift file (serviceA.thrift)
+	thriftFile := "../RPC_Server/serviceA.thrift"
+	existingContent, err := os.ReadFile(thriftFile)
+	assert.NoError(t, err)
+	err = os.WriteFile(thriftFile, newContent, 0644)
+	assert.NoError(t, err)
+
+	// Perform your test case and assertions here
+
+	body := `{"user": "test id", "file": "../RPC_Server/serviceA.thrift"}`
+	ctx := &app.RequestContext{
+		Request: *protocol.NewRequest(http.MethodGet, "/ServiceA/methodA", nil),
+	}
+	ctx.Request.SetHeader("Content-Type", "application/json")
+	ctx.Request.AppendBodyString(body)
+
+	testC := context.Background()
+
+	decode(testC, ctx)
+
+	assert.Equal(t, http.StatusAccepted, ctx.Response.StatusCode())
+
 	body2 := `{"user": "test id", "message": "test"}`
 	ctx2 := &app.RequestContext{
 		Request: *protocol.NewRequest(http.MethodGet, "/ServiceA/methodA", nil),
@@ -155,11 +182,17 @@ func TestIntegration4_ValidRequestWithUpdatedIDL(t *testing.T) {
 	ctx2.Request.AppendBodyString(body2)
 
 	testD := context.Background()
+	//initialise()
 	decode(testD, ctx2)
+	a := ctx2.Response.Body()
+	err = os.WriteFile(thriftFile, existingContent, 0644)
+	assert.NoError(t, err)
 
 	assert.Equal(t, http.StatusOK, ctx2.Response.StatusCode())
+	// After completing the test case and assertions, restore the original content
 
-	expected2 := `{"message":"User Connected to ServiceA, methodA.\nMessage content:test"}`
-	assert.Equal(t, expected2, string(ctx2.Response.Body()))
+	expected2 := `{"message":"Usertest id Connected to ServiceA, methodA.\nMessage content:test"}`
+	assert.Equal(t, expected2, string(a))
+
 }
 
